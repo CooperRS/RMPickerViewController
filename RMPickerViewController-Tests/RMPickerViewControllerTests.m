87290a63fef77533c38d19de399666fbfcf1aa22
CooperRS <CooperRS@yasit.net>
2014-01-29 11:15:00 +0100

    Added an example unit test

---
+++ RMPickerViewController-Tests/RMPickerViewControllerTests.m
@@ -0,0 +1,20 @@
+//
+//  RMPickerViewControllerTests.m
+//  RMPickerViewController-Demo
+//
+//  Created by Roland Moers on 29.01.14.
+//  Copyright (c) 2014 Roland Moers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RMPickerViewControllerTests : XCTestCase
+
+@end
+
+@implementation RMPickerViewControllerTests
+
+- (void)testExample {
+}
+
+@end
