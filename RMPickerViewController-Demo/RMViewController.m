e476b950d7a16f6f3402da7bd3a6b81fa0134c1b
CooperRS <cooperrs@gmail.com>
2014-08-22 10:47:18 +0200

    Demo project now is universal

--- RMPickerViewController-Demo/RMViewController.m
+++ RMPickerViewController-Demo/RMViewController.m
@@ -57,16 +57,36 @@
     //pickerVC.backgroundColor = [UIColor colorWithWhite:0.25 alpha:1];
     //pickerVC.selectedBackgroundColor = [UIColor colorWithWhite:0.4 alpha:1];
     
-    //The following lines show two ways to show the picker view controller:
-    // 1. Just show the picker view controller (make sure the delegate property is assigned)
-    [pickerVC show];
-    
-    // 2. Instead of using a delegate you can also pass blocks when showing the picker view controller
-    //[pickerVC showWithSelectionHandler:^(RMPickerViewController *vc, NSArray *selectedRows) {
-    //    NSLog(@"Successfully selected rows: %@ (With block)", selectedRows);
-    //} andCancelHandler:^(RMPickerViewController *vc) {
-    //    NSLog(@"Selection was canceled (with block)");
-    //}];
+    //The example project is universal. So we first need to check whether we run on an iPhone or an iPad.
+    if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        //OK, running on an iPhone. The following lines demonstrate the two ways to show the picker view controller on iPhones:
+        //(Note: These two methods also work an iPads.)
+        
+        // 1. Just show the picker view controller (make sure the delegate property is assigned)
+        [pickerVC show];
+        
+        // 2. Instead of using a delegate you can also pass blocks when showing the picker view controller
+        //[pickerVC showWithSelectionHandler:^(RMDateSelectionViewController *vc, NSDate *aDate) {
+        //    NSLog(@"Successfully selected date: %@ (With block)", aDate);
+        //} andCancelHandler:^(RMDateSelectionViewController *vc) {
+        //    NSLog(@"Date selection was canceled (with block)");
+        //}];
+    } else if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        //OK, running on an iPad. The following lines demonstrate the two special ways of showing the picker view controller on iPads:
+        
+        // 1. Show the picker view controller from a particular view controller (make sure the delegate property is assigned).
+        //    This method can be used to show the picker view controller within popovers.
+        //    (Note: We do not use self as the view controller, as showing a picker view controller from a table view controller
+        //           is not supported due to UIKit limitations.)
+        [pickerVC showFromViewController:self.navigationController];
+        
+        // 2. As with the two ways of showing the picker view controller on iPhones, we can also use a blocks based API.
+        //[pickerVC showFromViewController:self.navigationController withSelectionHandler:^(RMDateSelectionViewController *vc, NSDate *aDate) {
+        //    NSLog(@"Successfully selected date: %@ (With block)", aDate);
+        //} andCancelHandler:^(RMDateSelectionViewController *vc) {
+        //    NSLog(@"Date selection was canceled (with block)");
+        //}];
+    }
 }
 
 #pragma mark - UITableView Delegates
